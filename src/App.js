8317491832e8badab1943e51349ee4823c201361
Matthew Schlake <64107938+mschlake@users.noreply.github.com>
2020-10-26 12:15:25 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import PoemListPage from './PoemListPage'
+import PoemPage from './PoemPage'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <nav>
+          <Link to='/'>Poem List</Link>
+        </nav>
+        <header>
+          <h1>William Setstatespear</h1>
+        </header>
+        <main>
+          <Route 
+            exact path='/'
+            component={PoemListPage}
+          />
+          <Route
+            path='/poem/:poemId'
+            component={PoemPage}
+          />
+        </main>
+        <footer>
+          <p>© WilliamSetstatespear, 2018. All Rights Reserved.</p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
